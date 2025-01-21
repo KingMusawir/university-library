f81b67adf57516457042c9dd646eff12fab91b61
Musawir <musawir.raji@gmail.com>
2025-01-21 10:22:30 -0800

    fix email domain

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -26,7 +26,7 @@ export const sendEmail = async ({
       provider: resend({ token: config.env.resendToken }),
     },
     body: {
-      from: "Musawir University <hello.university-library.pro>",
+      from: "Musawir University <contact@musawirraji.pro>",
       to: [email],
       subject,
       html: message,
